64ee6dfb4846f91eacaa1f52332f45b64cc3e3ff
星星 <xingxing@xingxingdemacbook-pro.local>
2016-01-19 12:07:43 +0800

    新建项目

---
+++ NetEasyNews/NetEasyNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetEasyNews
+//
+//  Created by 星星 on 16/1/19.
+//  Copyright © 2016年 星星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
